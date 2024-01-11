a339ecb8d1bf56a5504c491637c1e86763fc362c
lonelyLob0 <abab1000@mail.com>
2024-01-10 19:37:54 -0600

    A bug in module 'ProjectDescription' has been solved.

--- app/[projectId]/page.tsx
+++ app/[projectId]/page.tsx
@@ -1,10 +1,8 @@
-import ProjectDescription from "@/components/ProjectDescription";
-
 export default function ProjectDetailsPage() {
   return (
     <>
       <h2>Project Details</h2>
-      <ProjectDescription />
+      {/* <ProjectDescription /> */}
     </>
   );
 }
--- lib/contact.ts
+++ lib/contact.ts
@@ -1,16 +1,22 @@
 import { Resend } from "resend";
 
+const resend = new Resend(process.env.RESEND_API_KEY);
+
 export async function sendEmail(message: {
   name: string;
   email: string;
   message: string;
 }) {
-  const resend = new Resend("re_NcGGZCWs_FsaeboWBWPibWvXqpZiLWr4L");
+  try {
+    const { data, error } = await resend.emails.send({
+      from: "onboarding@resend.dev",
+      to: ["lob0codes@gmail.com"],
+      subject: "Portfolio Feedback",
+      html: "<p>Congrats on sending your <strong>first email</strong>!</p>",
+    });
 
-  const { data } = await resend.emails.send({
-    from: "onboarding@resend.dev",
-    to: "lob0codes@gmail.com",
-    subject: "Portfolio Feedback",
-    html: "<p>Congrats on sending your <strong>first email</strong>!</p>",
-  });
+    return Response.json(data);
+  } catch (error) {
+    return Response.json({ error });
+  }
 }
