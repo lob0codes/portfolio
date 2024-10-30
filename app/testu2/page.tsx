ef0023747db74b0b307949325459b5e6fea7e08b
lob0codes <lob0codes@gmail.com>
2024-10-29 20:56:29 -0600

    Work started adding ProjectDetails page.

--- app/testu2/page.tsx
+++ app/testu2/page.tsx
@@ -1,10 +1,19 @@
-import SkillsTable from "@/components/Skills/SkillsTable";
 import classes from "@/app/testu2/page.module.css";
 
-export default function TestingPage() {
+export default function ProjectDetailsPage() {
   return (
-    <div className={classes.test}>
-      <SkillsTable />
+    <div className={classes.page}>
+      <header>Project Title Make it BIGG</header>
+      <ul className={classes.tabs}>
+        <li>General</li>
+        <li>Technologies</li>
+        <li>Gallery</li>
+      </ul>
+      <section className={classes.content}>
+        <div>Content of tab 1.</div>
+        <div>Content of tab 2.</div>
+        <div>Content of tab 3.</div>
+      </section>
     </div>
   );
 }
